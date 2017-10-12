16a5cc6a125abcf8f19886e27c8f9cde151d3819
Finnian Anderson <finnian.anderson@gmail.com>
2017-10-12 13:49:40 +0100

    Add OpenFaaS async functionality (#1)
    * Migrate to async functions
    
    Signed-off-by: Finnian Anderson <get@finnian.io>
    
    * Fix gateway url
    
    Signed-off-by: Finnian Anderson <get@finnian.io>
    
    * Add build & deploy scripts
    
    Signed-off-by: Finnian Anderson <get@finnian.io>
    
    * JSONify data before posting
    
    Signed-off-by: Finnian Anderson <get@finnian.io>
    
    * Rename listener
    
    Signed-off-by: Finnian Anderson <get@finnian.io>
    
    * Update debugging
    
    Signed-off-by: Finnian Anderson <get@finnian.io>
    
    * Fix request code
    
    Signed-off-by: Finnian Anderson <get@finnian.io>
    
    * Save image in minio instead of posting
    
    Signed-off-by: Finnian Anderson <get@finnian.io>
    
    * Fix import paths
    
    Signed-off-by: Finnian Anderson <get@finnian.io>
    
    * Fix json data post
    
    Signed-off-by: Finnian Anderson <get@finnian.io>

--- index.py
+++ index.py
@@ -1,35 +1,54 @@
-import os, json, time, tempfile, sys, io, urllib, requests
+import os, json, time, tempfile, sys, io, urllib, requests, contextlib
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
 
+from minio import Minio
+from minio.error import ResponseError
+
+@contextlib.contextmanager
+def nostdout():
+    save_stdout = sys.stdout
+    sys.stdout = io.BytesIO()
+    yield
+    sys.stdout = save_stdout
+
 auth = OAuthHandler(os.environ['consumer_key'], os.environ['consumer_secret'])
 auth.set_access_token(os.environ['access_token'], os.environ['access_token_secret'])
 
-class StdOutListener(StreamListener):
+minioClient = Minio(os.environ['minio_hostname'],
+                  access_key=os.environ['minio_access_key'],
+                  secret_key=os.environ['minio_secret_key'],
+                  secure=False)
+
+class TweetListener(StreamListener):
     def on_data(self, data):
-        print('Got tweet')
         tweet = json.loads(data)
-        print(tweet)
+        print('Got tweet from %s "%s" (%i followers)' % (tweet['user']['screen_name'], tweet['text'], tweet['user']['followers_count']))
         if (tweet['entities'] and tweet['entities']['media']):
-            print(tweet['entities']['media'])
             media = tweet['entities']['media'][0]
             if (media['type'] == 'photo'):
                 print("Ooooo a photo")
                 image_data = urllib.urlopen(media['media_url_https']).read()
 
-                r = requests.post(os.environ['colorization_url'], data=image_data)
-                if (r.status_code == requests.codes.ok):
-                    image_url = r.text.strip()
-                    print("Colorization succeeded for " + image_url)
-                    r = requests.post(os.environ['tweetpic_url'], data=json.dumps({
-                        "status_id": tweet['id_str'],
-                        "image": image_url
-                    }))
-                    if (r.status_code == requests.codes.ok):
-                        print('Successfully replied to %s with image %s' % (tweet['user']['screen_name'], image_url))
-                    else:
-                        print("Tweetback failed for image %s (%s)" % (image_url, r.text))
+                now = str(int(round(time.time() * 1000)))
+                filename_in = now + '.jpg'
+                file_path_in = tempfile.gettempdir() + '/' + filename_in
+
+                with open(file_path_in, 'wb') as f:
+                    f.write(image_data)
+
+                with nostdout():
+                    minioClient.fput_object('colorization', filename_in, file_path_in)
+
+                headers = {'X-Callback-Url': 'http://gateway:8080/async-function/tweetpic'}
+                json_data = {
+                    "image": filename_in,
+                    "status_id": tweet['id_str']
+                }
+                r = requests.post('http://gateway:8080/async-function/colorization', json=json_data, headers=headers)
+                if (r.status_code == requests.codes.accepted):
+                    print("Colorization succeeded for -> " + media['media_url_https'])
                 else:
                     print("Colorization failed for -> " + media['media_url_https'])
 
@@ -38,7 +57,7 @@ class StdOutListener(StreamListener):
 
 if __name__ == '__main__':
     print('Setting up')
-    l = StdOutListener()
+    l = TweetListener()
     stream = Stream(auth, l)
 
     print('Listening for tweets')
